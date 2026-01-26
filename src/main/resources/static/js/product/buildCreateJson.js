ccff8cd49fa4445cfc79dcd6ae31e549f174bc7a
114lutsenkoladimir <lucenkovaldymar@gmail.com>
2026-01-26 16:51:51 +0200

    .

---
+++ src/main/resources/static/js/product/buildCreateJson.js
@@ -0,0 +1,12 @@
+const name = document.getElementById();
+const description = document.getElementById();
+const groupId = document.getElementById();
+const releaseDate = document.getElementById();
+
+let parameters = [];
+
+parameters.push({
+    parameterId: parameterId,
+    value: value
+})
+
